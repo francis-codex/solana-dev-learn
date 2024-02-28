f6d566abe0e86ea8b9501750777effda382ae28c
francis-codex <francislogo2219@gmail.com>
2024-02-28 21:18:37 +0100

    loaded secret key from .env file, which apprently did not work, becos of dotenv/config

--- generate-keypair.ts
+++ generate-keypair.ts
@@ -1,6 +1,16 @@
-import { Keypair } from "@solana/web3.js";
-const keypair = Keypair.generate();
+import "dotenv/config"
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
-console.log(`The public key is:`, keypair.publicKey.toBase58());
-console.log(`The secret key is:`, keypair.secretKey);
-console.log(`✅ Finished`);
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`
+);
+
+// import { Keypair } from "@solana/web3.js";
+// const keypair = Keypair.generate();
+
+// console.log(`The public key is:`, keypair.publicKey.toBase58());
+// console.log(`The secret key is:`, keypair.secretKey);
+// console.log(`✅ Finished`);
+
